cd7de70241d490de930c1df68f87b2ef42b6fe6f
Dakota Hawkins <dakotahawkins0110@gmail.com>
2020-01-06 18:00:06 -0500

    Updatated constructors -- takes mutables vector defining mutable nodes.

--- include/SemiSupervisedRBCVertexPartition.h
+++ include/SemiSupervisedRBCVertexPartition.h
@@ -6,14 +6,16 @@
 class SemiSupervisedRBCVertexPartition : public RBConfigurationVertexPartition
 {
   public:
-    inline size_t mutable(size_t v) { return this -> _mutable[v]}
-    inline vector<size_t> const& mutable() const {return this -> _mutable[v]}
+    inline size_t mutables(size_t v) { return this -> _mutable[v]}
+    inline vector<size_t> const& mutables() const {return this -> _mutable[v]}
 
     SemiSupervisedRBCVertexPartition(Graph* graph,
                                      vector<size_t> const& membership,
-                                     double resolution_parameter);
+                                     double resolution_parameter
+                                     vector<bool> const& mutables);
     SemiSupervisedRBCVertexPartition(Graph* graph,
-                                     vector<size_t> const& membership);
+                                     vector<size_t> const& membership,
+                                     vector<bool> const& mutables);
     SemiSupervisedRBCVertexPartition(Graph* graph,
                                      double resolution_parameter);
     SemiSupervisedRBCVertexPartition(Graph* graph);
@@ -21,12 +23,14 @@ class SemiSupervisedRBCVertexPartition : public RBConfigurationVertexPartition
     virtual SemiSupervisedRBCVertexPartition* create(Graph* graph);
     virtual SemiSupervisedRBCVertexPartition* create(Graph* graph,
                                                      vector<size_t> const& membership);
-
+    // perhaps unnecessary if we're not modifying behavior
     virtual double diff_move(size_t v, size_t new_comm);
     virtual double quality(double resolution_parameter);
 
+    void set_mutable(vector<size_t> const& mutables);
+    void set_mutable(vector<size_t> const& mutables);
   protected:
-    vector<size_t> _mutable;
+    vector<bool> _mutables;
   private:
 };
 
